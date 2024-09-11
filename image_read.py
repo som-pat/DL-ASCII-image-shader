5fc6b3ccd435eb003815feb548ffe9eac3232a19
sompat <53874321+som-pat@users.noreply.github.com>
2024-09-12 00:03:29 +0530

    changes

--- image_read.py
+++ image_read.py
@@ -10,7 +10,19 @@ gray_shade = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 gray = cv2.normalize(gray_shade, None, 0, 255, cv2.NORM_MINMAX)
 
 width,height = gray.shape
-print(width,height)
+
+def downscaling(image,down_factor):
+    
+    down = cv2.resize(image, (0,0), fx=1/down_factor, fy=1/down_factor, interpolation=cv2.INTER_LINEAR) 
+    return down
+
+def upscaling(d_image,o_image):
+    
+    o_size = o_image.shape
+    upscale = cv2.resize(d_image,o_size,interpolation=cv2.INTER_NEAREST)
+
+    return upscale
+
 
 def ascii_conversion(gray):
     ascii_ren = []
@@ -36,4 +48,9 @@ def ascii_conversion(gray):
 
 # ascii_conversion(gray)
 
-print(img.shape)
+down_image = downscaling(gray,4)
+upscale_image = upscaling(down_image,gray) 
+ascii_conversion(upscale_image)
+# cv2.imshow('Downscaled Image', upscale_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
